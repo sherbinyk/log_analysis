0b51f948ec86ad4e6746bc82bc349ceeef138735
Khaled Elsherbiny <sherbinyk@gmail.com>
2017-07-10 08:04:56 +0200

    Applying PEP8 style, Python3 and some major changes to the code

--- newsdata.py
+++ newsdata.py
@@ -1,46 +1,41 @@
 #!/usr/bin/env python3
 import psycopg2 as p
 
-dbname = "news"
 
 def connect(news):
     try:
         db = p.connect("dbname={}".format(news))
-        c = db.cursor()
-        return db, c
+        cur = db.cursor()
+        return db, cur
     except psycopg2.Error as e:
         print
         'Unable to connect to database'
         raise e
-    db, c = connect()
 
 
-# Most popular three articles of all time
 def query1():
-    db = p.connect("dbname='news'")
-    cur = db.cursor()
+    """Most popular three articles of all time"""
+    db, cur = connect('news')
     cur.execute(
         "SELECT title, COUNT(*) AS views_count FROM articles "
-        "JOIN log ON articles.slug = (regexp_split_to_array(log.path, E'/article/'))[2] "
+        "JOIN log ON "
+        "articles.slug = (regexp_split_to_array(log.path, E'/article/'))[2] "
         "GROUP BY slug, title ORDER BY views_count DESC LIMIT 3;"
     )
 
     rows = cur.fetchall()
     print("\nTop 3 Articles")
     for article in rows:
-        print "Article: {0}     | {1} views".format(article[0], article[1])
+        print("Article: {0}     | {1} views".format(article[0], article[1]))
 
 
-query1()
-
-
-#  Most popular article authors of all time
 def query2():
-    db = p.connect("dbname='news'")
-    cur = db.cursor()
+    """Most popular article authors of all time"""
+    db, cur = connect('news')
     cur.execute(
         "SELECT authors.name, COUNT(authors.name) AS views FROM articles "
-        "JOIN authors ON authors.id = articles.author "
+        "JOIN authors ON "
+        "authors.id = articles.author "
         "JOIN log ON log.path LIKE '%' || articles.slug "
         "GROUP BY authors.name ORDER BY views DESC;"
     )
@@ -48,18 +43,14 @@ def query2():
     rows = cur.fetchall()
     print("\n\nTop authors")
     for author in rows:
-        print "Author name: {0}  | {1} views".format(author[0], author[1])
+        print("Author name: {0}  | {1} views".format(author[0], author[1]))
 
 
-query2()
-
-
-# Show on which days did more than 1% of requests lead to errors
 def query3():
-    db = p.connect("dbname='news'")
-    cur = db.cursor()
+    """Show on which days did more than 1% of requests lead to errors"""
+    db, cur = connect('news')
     cur.execute(
-        "SELECT f.date AS date, f.count AS Fail_Req, a.count AS All_Req, "
+        "SELECT f.date AS date, "
         "(f.count::float) *100 / (a.count::float) AS percent "
         "FROM fail AS f JOIN total AS a ON f.date = a.date "
         "WHERE (f.count::float) *100 / (a.count::float) > 1 "
@@ -69,7 +60,12 @@ def query3():
     rows = cur.fetchall()
     print("\n\nErrors in Percent")
     for r in rows:
-        print "Date: {0}  | Failed Requests: {1}   | Total Requests: {2} | Error in Percentage: {3}%".format(r[0], r[1], r[2], r[3])
+        print("Date: {0:%B %d, %Y} \
+                | Error in Percentage: {1:.2f}%"
+              .format(r[0], r[1]))
 
 
-query3()
+if __name__ == "__main__":
+    query1()
+    query2()
+    query3()
